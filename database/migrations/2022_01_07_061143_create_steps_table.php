354dfcc1e270d6ddbda4682e54523a07a3a620cb
Jason <jason.chen@accuhit.net>
2022-01-07 16:04:01 +0800

    adding migration schema

---
+++ database/migrations/2022_01_07_061143_create_steps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steps', function (Blueprint $table) {
+            $table->id();
+            $table->string('step', 20)->index()->comment('活動階段');
+            $table->string('answer', 20)->nullable()->comment('選擇答案');
+            $table->integer('count')->default('1')->comment('次數');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('建立時間');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('更新時間');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steps');
+    }
+}
---
+++ database/migrations/2022_01_07_061155_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('姓名');
+            $table->string('title')->comment('職稱');
+            $table->string('company')->comment('公司名稱');
+            $table->string('tel', 20)->comment('公司電話');
+            $table->string('mobile', 20)->index()->comment('行動電話');
+            $table->text('email')->comment('Email');
+            $table->enum('sales', ['Y', 'N'])->comment('願意業務聯繫');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('建立時間');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))->comment('更新時間');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
