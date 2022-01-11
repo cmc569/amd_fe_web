82cb50f98c8f1cdb7ba96addb0bfdae33ae84272
Jason <jason.chen@accuhit.net>
2022-01-11 18:38:06 +0800

    modify active for sidebar

--- resources/views/admin/layout/_sidebar.blade.php
+++ resources/views/admin/layout/_sidebar.blade.php
@@ -14,8 +14,8 @@
           <button type="button" class="btn btn-info" onclick="logout()">登出</button><br>
         </div>
         <div class="sidebar__nav">
-          <a href="{{ route('dashboard') }}" class="nav-list active"><i class="far fa-fw fa-chart-bar"></i>活動網頁數據</a>
-          <a href="{{ route('userInfo') }}" class="nav-list"><i class="fas fa-fw fa-list"></i>使用者資訊</a>
+          <a href="{{ route('dashboard') }}" class="nav-list{{ request()->routeIs('dashboard') ? ' active' : ''}}"><i class="far fa-fw fa-chart-bar"></i>活動網頁數據</a>
+          <a href="{{ route('userInfo') }}" class="nav-list{{ request()->routeIs('userInfo') ? ' active' : ''}}"><i class="fas fa-fw fa-list"></i>使用者資訊</a>
         </div>
       </div>
     </aside>
