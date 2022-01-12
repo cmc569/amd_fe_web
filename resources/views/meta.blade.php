59a5baec45e6293d064876a6a1d98b8632f51981
Jason <jason.chen@accuhit.net>
2022-01-12 16:33:09 +0800

    adding meta property

---
+++ resources/views/meta.blade.php
@@ -0,0 +1,7 @@
+    
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="你是哪一種類型的職場高手">
+    <!-- <meta property="og:description" content="線上即刻抽學運籤，還有機會拿神明加持神助攻考試組" /> -->
+    <!-- <meta property="og:image" content="https://ua-fb-api.accuhit.com.tw/images/result_share_detail_1.jpg" /> -->
+    <!-- <meta property="og:image:width" content="1200" /> -->
+    <!-- <meta property="og:image:height" content="630" /> -->
--- resources/views/result0.blade.php
+++ resources/views/result0.blade.php
@@ -2,6 +2,8 @@
 <html lang="zh-TW">
 
 <head>
+  @include('meta')
+  
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
