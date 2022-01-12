54a66001d844d8a9170f8c5e2eaaac89b45170a8
Jason <jason.chen@accuhit.net>
2022-01-12 17:04:47 +0800

    adding og image property

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -1,7 +1,7 @@
     
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="你是哪一種類型的職場高手">
-    <!-- <meta property="og:description" content="線上即刻抽學運籤，還有機會拿神明加持神助攻考試組" /> -->
-    <!-- <meta property="og:image" content="https://ua-fb-api.accuhit.com.tw/images/result_share_detail_1.jpg" /> -->
-    <!-- <meta property="og:image:width" content="1200" /> -->
-    <!-- <meta property="og:image:height" content="630" /> -->
+    <meta property="og:title" content="你是哪一種類型的職場高手－AMD｜unitech">
+    <meta property="og:description" content="你是哪一種類型的職場高手，是效率、規劃、溝通還是創意專家？參與測驗了解更多>>" />
+    <meta property="og:image" content="/img/fbshare_banner.jpg" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="720" />
