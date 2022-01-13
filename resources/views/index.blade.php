75ece075e7987b863b90dab0610e50d28d588496
Jason <jason.chen@accuhit.net>
2022-01-13 10:04:04 +0800

    remove meta blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,13 @@
 <html lang="zh-TW">
 
 <head>
-	@include('meta')
-
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="你是哪一種類型的職場高手－AMD｜unitech">
+    <meta property="og:description" content="你是哪一種類型的職場高手，是效率、規劃、溝通還是創意專家？參與測驗了解更多>>" />
+    <meta property="og:image" content="/img/fbshare_banner.jpg" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="720" />
+	
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
--- resources/views/meta.blade.php
+++
@@ -1,7 +0,0 @@
-    
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="你是哪一種類型的職場高手－AMD｜unitech">
-    <meta property="og:description" content="你是哪一種類型的職場高手，是效率、規劃、溝通還是創意專家？參與測驗了解更多>>" />
-    <meta property="og:image" content="/img/fbshare_banner.jpg" />
-    <meta property="og:image:width" content="1280" />
-    <meta property="og:image:height" content="720" />
