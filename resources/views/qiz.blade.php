bff31ade9f4915879a1025a87a07ff5aef6ab36f
Jason <jason.chen@accuhit.net>
2022-01-13 11:20:19 +0800

    modify js version

--- resources/views/qiz.blade.php
+++ resources/views/qiz.blade.php
@@ -179,7 +179,7 @@
     crossorigin="anonymous"></script>
   <!-- <script src="https://unpkg.com/aos@next/dist/aos.js"></script> -->
   <script src="js/jquery.validate.min.js"></script>
-  <script src='/js/main.js?v=3'></script>
+  <script src='/js/main.js?v=4'></script>
   <script>
 
     $(document).ready(function () {
